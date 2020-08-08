ea2b89580821cbe5a9dfbc82bd3b59996d78d692
Fei Zhang <zhangf37@dev-intel14.i>
2020-08-08 15:51:36 -0400

    results

--- stat_results/mapping_stat.py
+++ stat_results/mapping_stat.py
@@ -3,17 +3,21 @@ import sys,os,glob,re
 inpath = sys.argv[1]
 infiles = glob.glob(inpath+'/*summary.txt')
 
-outputtxt = 'sample,alignment rate,concordantly 1 time,concordantly >1 time\n'
+outputtxt = 'sample,total reads,paired unique mapped reads,alignment rate,concordantly 1 time,concordantly >1 time\n'
 for statfile in infiles:
     filename = statfile.split('/')[-1]
     samplename = filename.split('_')[-3]
     txt = open(statfile,'r').read()
-    m = re.search('([^\s]+?%) overall alignment rate', txt)
+    m = re.search('([^\s]+?) reads; of these:', txt)
     m1 = m.group(1)
-    m = re.search('\((.+?%)\) aligned concordantly exactly 1 time', txt)
+    m = re.search('([^\s]+?) \(.+\) aligned concordantly exactly 1 time', txt)
     m2 = m.group(1)
-    m = re.search('\((.+?%)\) aligned concordantly >1 times', txt)
+    m = re.search('([^\s]+?%) overall alignment rate', txt)
     m3 = m.group(1)
-    outputtxt += '%s,%s,%s,%s\n' % (samplename, m1, m2, m3)
+    m = re.search('\((.+?%)\) aligned concordantly exactly 1 time', txt)
+    m4 = m.group(1)
+    m = re.search('\((.+?%)\) aligned concordantly >1 times', txt)
+    m5 = m.group(1)
+    outputtxt += '%s,%s,%s,%s,%s,%s\n' % (samplename, m1, m2, m3, m4, m5)
 
 open('mappingstat.csv','w').write(outputtxt)
